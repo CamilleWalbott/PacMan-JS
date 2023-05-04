e54ba16b30f1957f66a16c42e1cbbc278716ccfb
Camille Walbott <camille.walbott@gmail.com>
2023-05-04 23:12:29 +0200

    Création Map

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-//Réglages canvas
+//Réglages canvas / Canvas settings
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+//Création map / map creation
+
+//classe pour un carré de la map
+class Boundary {
+    static width = 40
+    static height = 40
+    constructor({position}  ){
+      this.position = position 
+      this.width = 40
+      this.height = 40
+    }
+
+    draw(){
+        c.fillStyle ='blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+//dessin map 
+const map = [
+  ['-', '-', '-', '-', '-', '-',], 
+  ['-', ' ', ' ', ' ', ' ', '-',], 
+  ['-', ' ', '-', '-', ' ', '-',],
+  ['-', ' ', ' ', ' ', ' ', '-',], 
+  ['-', '-', '-', '-', '-', '-',], 
+]
+const boundaries = []
+
+map.forEach((row, i) => {
+    row.forEach((symbol, j) =>{
+      switch(symbol){
+        case '-': 
+        boundaries.push(
+            new Boundary({
+                position: {
+                    x: Boundary.width *j,
+                    y: Boundary.height *i
+                }
+            })
+        )
+        break
+      }  
+    })
+})
+boundaries.forEach((boundary) => {
+    boundary.draw();
+})
